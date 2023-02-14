a5e2e2f4c450b63bd5ac41e08f58ad96ee2aab83
mariodemoder <mariodemoder@gmail.com>
2023-02-14 10:50:41 -0300

    ajuste a env que no se vera

--- config/database.php
+++ config/database.php
@@ -63,7 +63,7 @@ return [
             ]) : [],
         ],
 
-        'faltas' => [
+        'mysql1' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST1', '127.0.0.1'),
